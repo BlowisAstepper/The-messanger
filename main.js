ca0f2dad37e4b8f4d2921de20dc0ec291fe61f27
Dave <davidotoo09@gmail.com>
2025-10-23 22:27:10 +0000

    changes to userAge

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout 
+  output: process.stdout
 });
 
-let userAge;
+const currentYear = new Date().getFullYear();
 
 rl.question("What's your name? ", (name) => {
   console.log(`Hello, ${name}!`);
 
   rl.question("Which year were you born? ", (year) => {
-    const birthYear = Number(year); // make sure it's a number
+    const birthYear = Number(year);
 
-    if (birthYear > 2025) {
-      console.log("Dumbass, did you think I wouldn’t know... smh!!");
-    } else {
-      userAge = 2025 - birthYear;
-     // console.log(`Thanks for the information. You're about ${userAge} years old.`);
+    if (isNaN(birthYear)) {
+      console.log("Please enter a valid year.");
+      rl.close();
+      return;
+    }
+
+    if (birthYear > currentYear) {
+      console.log("That year is in the future.");
+      rl.close();
+      return;
+    }
+
+    const userAge = currentYear - birthYear;
+    console.log(`Thanks for the information. You're about ${userAge} years old.`);
+
+    if (birthYear < 1990) {
+      console.log("You aint that old come on");
     }
 
     rl.close();
