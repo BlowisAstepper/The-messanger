7dd26b27991cce68bbdf8d0de830b780b440470f
Dave <davidotoo09@gmail.com>
2025-10-23 15:46:14 +0000

    username and age

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-const readline = require('readline')
+const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout 
+  input: process.stdin,
+  output: process.stdout 
 });
 
-rl.question("What's your name?" , (answer) =>{
-    console.log(`Hello, ${answer}!`)
+let userAge;
+
+rl.question("What's your name? ", (name) => {
+  console.log(`Hello, ${name}!`);
+
+  rl.question("Which year were you born? ", (year) => {
+    const birthYear = Number(year); // make sure it's a number
+
+    if (birthYear > 2025) {
+      console.log("Dumbass, did you think I wouldn’t know... smh!!");
+    } else {
+      userAge = 2025 - birthYear;
+     // console.log(`Thanks for the information. You're about ${userAge} years old.`);
+    }
+
     rl.close();
+  });
 });
